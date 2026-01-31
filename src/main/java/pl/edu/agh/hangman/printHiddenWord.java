5aa917a6410706cde17c54fd09001a0c3455e1b4
rpole <rpolek85@gamil.com>
2026-01-31 10:38:02 +0100

    PrintHangman

---
+++ src/main/java/pl/edu/agh/hangman/printHiddenWord.java
@@ -0,0 +1,14 @@
+package pl.edu.agh.hangman;
+
+public class printHiddenWord {
+
+    public static String getHiddenWord(String word) {
+        if (word == null) return "";
+
+        StringBuilder hidden = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            hidden.append("_ ");
+        }
+        return hidden.toString();
+    }
+}
