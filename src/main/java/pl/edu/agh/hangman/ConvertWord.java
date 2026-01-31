8cd8d786139ca6526e27998aa02edfce142ae412
mkulpinsk <m.machowsk@gmail.com>
2026-01-31 10:42:45 +0100

    utworzenie klasy ConvertWord

---
+++ src/main/java/pl/edu/agh/hangman/ConvertWord.java
@@ -0,0 +1,29 @@
+package pl.edu.agh.hangman;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ConvertWord {
+
+    private String src;
+    private String filePath = "src/main/resources/slowa.txt";
+
+    public List<String> readWordsFromFile(String filePath) {
+        List<String> words = new ArrayList<>();
+
+        try (Stream<String> stream = Files.lines(Paths.get(filePath))) {
+            words = stream
+                    .flatMap(line -> Stream.of(line.split("\\s+")))
+                    .filter(word -> !word.isEmpty())
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return words;
+    }
+}
