c74ccfa67ee43d30fa30787561e86b0fd458ac86
mszpunar <mszpunar@d17-426a-pc-03.ipa.ii.agh.edu.pl>
2026-01-31 11:03:19 +0100

    dodanie obsługi listLetter

--- src/main/java/pl/edu/agh/hangman/PrintHiddenWord.java
+++ src/main/java/pl/edu/agh/hangman/PrintHiddenWord.java
@@ -1,14 +1,26 @@
 package pl.edu.agh.hangman;
 
+import java.util.List;
+
 public class PrintHiddenWord {
 
-    public static String getHiddenWord(String word) {
+    public static String getHiddenWord(String word, List<String> listLetter) {
         if (word == null) return "";
 
         StringBuilder hidden = new StringBuilder();
+
         for (int i = 0; i < word.length(); i++) {
-            hidden.append("_ ");
+            char currentLetter = word.charAt(i);
+
+            String letterStr = String.valueOf(currentLetter);
+
+            if (listLetter.contains(letterStr)) {
+                hidden.append(currentLetter).append(" ");
+            } else {
+                hidden.append("_ ");
+            }
         }
-        return hidden.toString();
+
+        return hidden.toString().trim();
     }
 }
